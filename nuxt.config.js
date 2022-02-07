37928e13e602be370b58a508f87445fe97d6516f
Drew <drewclementsdesign@gmail.com>
2022-02-07 13:13:35 -0500

    add axios and build processes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const isPreview = process.env.NODE_ENV === 'development';
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -26,6 +29,61 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  generate: {
+    concurrency: 25,
+    fallback: true,
+    crawler: false, // Revisit in the future
+    routes: function (callback) {
+      const token = process.env.STORYBLOK_API_KEY;
+      const version = isPreview ? 'draft' : 'published';
+      let cacheVersion = 0;
+      // ignore these files and folders
+      const ignoreFiles = ['home', 'global', 'sitefooter'];
+      // update to remove docs after docs build!
+      const ignoreFolders = ['authors', 'companies', 'releases'];
+
+      const routes = ['/'];
+
+      const getRoutes = async (ignoreFiles, ignoreFolders) => {
+        axios
+          .get(`https://api.storyblok.com/v1/cdn/spaces/me?token=${token}`)
+          /* eslint-disable-next-line camelcase */
+          .then((space_res) => {
+            // timestamp of latest publish
+            cacheVersion = space_res.data.space.version;
+            // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
+            axios
+              .get(
+                `https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&cv=${cacheVersion}`
+              )
+              .then((res) => {
+                Object.keys(res.data.links).forEach((key) => {
+                  if (
+                    !ignoreFiles.includes(res.data.links[key].slug) &&
+                    !ignoreFolders.includes(res.data.links[key].slug.split('/')[0])
+                  ) {
+                    /*
+                     * This block isn't pretty but it prevents attempts
+                     * to generate the index.html file of folders that don't
+                     * have root files in Storyblok. (No index.html.)
+                     */
+                    if (!(res.data.links[key].is_folder && !res.data.links[key].is_startpage)) {
+                      routes.push('/' + res.data.links[key].slug);
+                    }
+                  }
+                });
+
+                callback(null, routes);
+              });
+          });
+      };
+
+      getRoutes(ignoreFiles, ignoreFolders);
+
+      return routes;
+    },
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
