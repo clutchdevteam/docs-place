49557d0f69fd1392c91b07b222115c5460fadaf4
Drew <drewclementsdesign@gmail.com>
2022-02-09 16:19:19 -0500

    casing error

--- plugins/rich-text-renderer.js
+++ plugins/rich-text-renderer.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { Block } from '@marvr/storyblok-rich-text-types';
 import RichTextHeading from '../components/base/BaseRichText/Heading.vue';
-import RichTextParagraph from '../components/Base/BaseRichText/Paragraph.vue';
+import RichTextParagraph from '../components/base/BaseRichText/Paragraph.vue';
 import VueRichTextRenderer from '@marvr/storyblok-rich-text-vue-renderer';
 
 Vue.use(VueRichTextRenderer, {
