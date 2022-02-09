4af7e0cbf3ce0bdd916a6ab5fd12c4e71e7915b4
Drew <drewclementsdesign@gmail.com>
2022-02-09 16:05:52 -0500

    trying to fix breaking merge

--- plugins/rich-text-renderer.js
+++ plugins/rich-text-renderer.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import { Block } from '@marvr/storyblok-rich-text-types';
-import RichTextHeading from '~/components/Base/BaseRichText/RichTextHeading';
-import RichTextParagraph from '~/components/Base/BaseRichText/RichTextParagraph';
+import RichTextHeading from '~/components/Base/BaseRichText/RichTextHeading.vue';
+import RichTextParagraph from '~/components/Base/BaseRichText/RichTextParagraph.vue';
 import VueRichTextRenderer from '@marvr/storyblok-rich-text-vue-renderer';
 
 Vue.use(VueRichTextRenderer, {
