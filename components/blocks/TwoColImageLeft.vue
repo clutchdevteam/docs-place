075692d86c345b0c5bd3a2a766530475a1435707
Drew <drewclementsdesign@gmail.com>
2022-02-09 14:04:33 -0500

    improve storybook controls

--- components/blocks/TwoColImageLeft.vue
+++ components/blocks/TwoColImageLeft.vue
@@ -1,24 +1,26 @@
 <template>
-  <section class="base-wrapper">
+  <section class="base-wrapper my-24 md:my-36">
     <BaseTwoCol>
       <template v-slot:left>
         <div class="relative">
           <BaseImage :src="block.image.filename" :alt="block.image.alt" />
           <div
-            :class="`decor-square absolute bottom-0 right-0 mb-[-10%] mr-[-10%] md:mb-[-20%] md:mr-[-10%] w-24 h-24 md:w-1/2 md:h-1/2 ${block.theme}`"
+            :class="`decor-square absolute bottom-0 right-0 mb-[-10%] mr-[-5%] md:mb-[-20%] md:mr-[-10%] w-24 h-24 md:w-1/2 md:h-1/2 ${block.theme}`"
           />
         </div>
       </template>
 
       <template v-slot:right>
-        <BaseHeading class="text-primary-dark font-bold" size="h3">
-          {{ block.heading }}
-        </BaseHeading>
+        <div :class="{ 'mt-8': block.heading }">
+          <BaseHeading v-if="block.heading" class="text-primary-dark font-bold" size="h3">
+            {{ block.heading }}
+          </BaseHeading>
 
-        <div class="md:w-3/4">
-          <BaseText>
-            {{ block.text }}
-          </BaseText>
+          <div class="lg:w-3/4">
+            <BaseText>
+              {{ block.text }}
+            </BaseText>
+          </div>
         </div>
       </template>
     </BaseTwoCol>
