4fb6fe792412c4ae28840501c44214a8865c9a7a
Drew <drewclementsdesign@gmail.com>
2022-02-10 22:59:14 -0500

    maybe?

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -19,6 +19,7 @@
             method="POST"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
+            action="/thanks"
           >
             <BaseInput class="mb-4" v-model="name" name="name" required>Name</BaseInput>
 
@@ -66,6 +67,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data() {
     return {
@@ -76,6 +79,36 @@ export default {
       responseMessage: null,
       responseState: null,
     };
+  },
+  computed: {
+    ...mapState('global', ['loaded']),
+    // set version based on environment
+    version() {
+      return this.$nuxt.context.query._storyblok || this.$nuxt.context.isDev
+        ? 'draft'
+        : 'published';
+    },
+  },
+  mounted() {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge();
+
+      // Use the input event for instant update of content
+      storyblokInstance.on('input', (event) => {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content;
+        }
+      });
+
+      // Use the bridge to listen the events
+      storyblokInstance.on(['input', 'published', 'change'], (event) => {
+        // window.location.reload()
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        });
+      });
+    });
   },
   methods: {
     encode(data) {
@@ -106,6 +139,38 @@ export default {
         })
         .catch((e) => console.error(e));
     },
+  },
+  async fetch() {
+    const globalRes = await this.$storyapi.get('cdn/stories/global', {
+      version: this.version,
+    });
+
+    // set global content in vuex
+    this.$store.commit('global/setGlobals', globalRes.data.story.content);
+    // set loaded to true to negate uneccesary additional calls to storyblok
+    this.$store.commit('global/isLoaded', true);
+
+    const fullSlug = this.$route.path === '/' ? 'home' : this.$route.path;
+
+    let res;
+    try {
+      res = await this.$storyapi.get(`cdn/stories/${fullSlug}`, {
+        version: this.version,
+      });
+      this.story = res.data.story;
+    } catch (e) {
+      if (!e.response) {
+        this.$nuxt.context.error({
+          statusCode: 404,
+          message: 'Failed to receive content from api',
+        });
+      } else {
+        this.$nuxt.context.error({
+          statusCode: e.response.status,
+          message: e.response.data,
+        });
+      }
+    }
   },
 };
 </script>
--- pages/thanks.vue
+++ pages/thanks.vue
@@ -75,6 +75,98 @@ export default {
       responseState: 'success',
     };
   },
+  computed: {
+    ...mapState('global', ['loaded']),
+    // set version based on environment
+    version() {
+      return this.$nuxt.context.query._storyblok || this.$nuxt.context.isDev
+        ? 'draft'
+        : 'published';
+    },
+  },
+  mounted() {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge();
+
+      // Use the input event for instant update of content
+      storyblokInstance.on('input', (event) => {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content;
+        }
+      });
+
+      // Use the bridge to listen the events
+      storyblokInstance.on(['input', 'published', 'change'], (event) => {
+        // window.location.reload()
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        });
+      });
+    });
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+        .join('&');
+    },
+    handleSubmit() {
+      fetch('/', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': 'contact',
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          message: this.message,
+        }),
+      })
+        .then((res, err) => {
+          if (res.status === 200) {
+            this.responseMessage = "Thanks for reaching out! We'll be in contact shortly!";
+            this.responseState = 'success';
+          } else {
+            this.responseMessage = 'Oops! Looks like something went wrong. Please try again!';
+            this.responseState = 'error';
+          }
+        })
+        .catch((e) => console.error(e));
+    },
+  },
+  async fetch() {
+    const globalRes = await this.$storyapi.get('cdn/stories/global', {
+      version: this.version,
+    });
+
+    // set global content in vuex
+    this.$store.commit('global/setGlobals', globalRes.data.story.content);
+    // set loaded to true to negate uneccesary additional calls to storyblok
+    this.$store.commit('global/isLoaded', true);
+
+    const fullSlug = this.$route.path === '/' ? 'home' : this.$route.path;
+
+    let res;
+    try {
+      res = await this.$storyapi.get(`cdn/stories/${fullSlug}`, {
+        version: this.version,
+      });
+      this.story = res.data.story;
+    } catch (e) {
+      if (!e.response) {
+        this.$nuxt.context.error({
+          statusCode: 404,
+          message: 'Failed to receive content from api',
+        });
+      } else {
+        this.$nuxt.context.error({
+          statusCode: e.response.status,
+          message: e.response.data,
+        });
+      }
+    }
+  },
 };
 </script>
 
