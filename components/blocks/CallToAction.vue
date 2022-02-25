1163a31b33ba81b4d4af88712deb3dfb95413d45
Matt Clark <reddevdesign@gmail.com>
2022-02-25 13:12:03 -0500

    fixed mobile layout

--- components/blocks/CallToAction.vue
+++ components/blocks/CallToAction.vue
@@ -1,23 +1,23 @@
 <template>
-  <section class="flex flex-col justify-center items-center">
-    <div class="bg-primary-dark">
-      <BaseImage
-        class="w-screen h-auto opacity-10"
-        :src="block.background_image.filename"
-        :alt="block.background_image.alt"
-      />
-    </div>
+  <section
+    class="flex flex-col justify-center items-center relative py-20 bg-primary-dark"
+  >
+    <BaseImage
+      class="object-cover h-full w-auto absolute opacity-10 z-0 md:w-full md:h-max"
+      :src="block.background_image.filename"
+      :alt="block.background_image.alt"
+    />
 
     <div
-      class="base-wrapper flex flex-col justify-center items-center opacity-100 absolute"
+      class="base-wrapper flex flex-col justify-center md:items-center opacity-100 z-10"
     >
-      <div
-        class="text-base text-center font-bold text-white my-2 md:text-3xl md:my-4 lg:text-5xl lg:my-8"
+      <p
+        class="text-2xl leading-9 text-left font-bold w-3/4 text-white mb-8 md:text-4xl md:leading-normal md:w-full md:my-4 md:text-center lg:text-5xl lg:leading-normal lg:w-11/12 lg:my-8"
       >
         {{ block.text }}
-      </div>
-      <div>
-        <BaseButton class="my-2 md:my-4 lg:my-8">
+      </p>
+      <div class="flex flex-col items-center">
+        <BaseButton class="w-full my-8 md:my-4 lg:my-8">
           {{ block.button[0].btnText }}
         </BaseButton>
       </div>
